c1bf1a55c319edc43270b1583a497e7700df6906
SamKnyarko <sknyarko010@st.ug.edu.gh>
2025-08-05 00:56:12 +0100

    Commit 2 a  menu driven interface

--- DCIT318Assignment1/Program.cs
+++ DCIT318Assignment1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\n==== DCIT 318 Assignment Menu ====");
+            Console.WriteLine("1. Grade Calculator");
+            Console.WriteLine("2. Ticket Price Calculator");
+            Console.WriteLine("3. Triangle Type Identifier");
+            Console.WriteLine("4. Exit");
+            Console.Write("Select an option (1-4): ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    GradeCalculator();
+                    break;
+                case "2":
+                    TicketPriceCalculator();
+                    break;
+                case "3":
+                    TriangleTypeIdentifier();
+                    break;
+                case "4":
+                    Console.WriteLine("Exiting...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Try again.");
+                    break;
+            }
+        }
+    }
+
+   
