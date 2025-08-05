79688bf07da63879c5be3a7772f6ad370d08e3e1
SamKnyarko <sknyarko010@st.ug.edu.gh>
2025-08-05 01:14:17 +0100

    Ticket price calculator added

--- DCIT318Assignment1/Program.cs
+++ DCIT318Assignment1/Program.cs
@@ -52,4 +52,19 @@ class Program
         }
     }
 
+    static void TicketPriceCalculator()
+    {
+        Console.Write("Enter your age: ");
+        if (int.TryParse(Console.ReadLine(), out int age))
+        {
+            int price = (age <= 12 || age >= 65) ? 7 : 10;
+            Console.WriteLine($"Ticket Price: GHC{price}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input.");
+        }
+    }
+
+
 
