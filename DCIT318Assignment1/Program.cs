7901aaa58536b2ac82d085b510af461069782f8c
SamKnyarko <sknyarko010@st.ug.edu.gh>
2025-08-05 01:32:41 +0100

    TriangleTypeIdentifier ADDED

--- DCIT318Assignment1/Program.cs
+++ DCIT318Assignment1/Program.cs
@@ -66,5 +66,22 @@ class Program
         }
     }
 
+    static void TriangleTypeIdentifier()
+    {
+        Console.Write("Enter side 1: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Enter side 2: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("Enter side 3: ");
+        double c = double.Parse(Console.ReadLine());
+
+        if (a == b && b == c)
+            Console.WriteLine("Triangle Type: Equilateral");
+        else if (a == b || b == c || a == c)
+            Console.WriteLine("Triangle Type: Isosceles");
+        else
+            Console.WriteLine("Triangle Type: Scalene");
+    }
+}
 
 
